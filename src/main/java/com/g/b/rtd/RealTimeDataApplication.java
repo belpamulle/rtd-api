521da777e82ed08d5bced9eda838f7967dd6e78a
Gayan B <gayan.belpamulle@mantelgroup.com.au>
2024-11-13 19:18:55 +1100

    initial project setup with https://start.spring.io/ for GraphQL server with Springboot

---
+++ src/main/java/com/g/b/rtd/RealTimeDataApplication.java
@@ -0,0 +1,13 @@
+package com.g.b.rtd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g/b/rtd/RealTimeDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g.b.rtd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
